654e57d93736ea63cc164e20e89baccdbadd963e
DasBlinkenlights <er.ilgaz@gmail.com>
2017-08-25 15:11:06 +0300

    Initial commit

--- scripts/main.js
+++ scripts/main.js
@@ -24,11 +24,17 @@ function PageScript() {
 
 PageScript.prototype.onSelectBolge = function() {
 	this.bitkiSelect.empty();
-	this.bitkiSelect.append('<option value=""></option>')
+	this.bitkiSelect.append('<option value=""></option>');
+	this.organikSelect.empty();
+	this.organikSelect.html('<option value=""></option>' + 
+            				"<option value=1>0 – 1.0</option>" + 
+            				"<option value=2>1.1 – 2.0</option>" + 
+            				"<option value=3>2.1 – 3.0</option>" + 
+            				"<option value=4>3+</option>");
 	this.sonucBox.empty();
 	this.bitki.hide();
 	this.organik.hide();
-	this.sonuc.hide()
+	this.sonuc.hide();
 	Papa.parse("data/"+this.bolgeSelect.val()+"-azot.csv", {
 		download: true,
 		complete: function(results) {
@@ -45,20 +51,32 @@ PageScript.prototype.onSelectBolge = function() {
 	});	
 };
 PageScript.prototype.onSelectBitki = function() {
+	if(this.bitkiSelect.val()==''){
+		this.organik.hide();
+		this.sonuc.hide();
+		return;
+	}
+	/*this.organikSelect.empty();
+	this.organikSelect.html('<option value=""></option>' + 
+            				"<option value=1>0 – 1.0</option>" + 
+            				"<option value=2>1.1 – 2.0</option>" + 
+            				"<option value=3>2.1 – 3.0</option>" + 
+            				"<option value=4>3+</option>");*/
 	this.bitkiData=this.bolgeData[this.bitkiSelect.val()];
 	this.organik.show();
 	this.onSelectOrganik();
 };
 PageScript.prototype.onSelectOrganik = function() {
-	if(this.organikSelect.val()=='NO-OP'){
+	if(this.organikSelect.val()==''){
+		this.sonuc.hide();
 		return;
 	}
-	this.sonuc.show();
 	this.sonucBox.empty();
+	this.sonuc.show();
 	let safazot = parseFloat(this.bitkiData[this.organikSelect.val()]);
 	for(let i=0;i<this.gubre.length-1; i++) {
 		let katsayi = parseFloat(this.gubre[i][1])
-		this.sonucBox.append(this.gubre[i][0] + ": " + safazot*katsayi + "kg / 1 dekar (1 dönüm) arazi<br><br>");
+		this.sonucBox.append(this.gubre[i][0] + " : " + safazot*katsayi + "kg / 1 dekar (1 dönüm) arazi<br><br>");
 	}
 }
 
